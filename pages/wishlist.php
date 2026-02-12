c69b6ce29bfc67ed65fa10b840264f159a32a7f7
KMV531 <koladjamomovinnybrayan3@gmail.com>
2026-02-12 16:10:28 +0100

    feat: implement semantic header with search and user navigation

---
+++ pages/wishlist.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AniWix - Vos Favoris</title>
+    <link rel="icon" href="../assets/favicon.png" type="image/png" />
+    <link rel="stylesheet" href="../styles/index.css" />
+</head>
+<body>
+    <?php include("../components/header.php") ?>
+    <h1>Welcome</h1>
+</body>
+</html>
